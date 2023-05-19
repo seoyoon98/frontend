7b29849eaf0852e1e0563c7f147be05a4a240e24
Suhyeon <fbtngus2789@naver.com>
2023-05-19 17:54:04 +0900

    Header 제작 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,18 +6,29 @@ const Header = () => {
   const [login, setLogin] = useState(false);
 
   return (
-    <div className="text-3xl flex justify-between items-center px-8 h-24 bg-orange-300/[0.75] text-black">
-      <a href="/">
+    <div className="text-3xl flex justify-between items-center px-8 h-20 bg-orange-300/[0.75] text-black">
+      <a href="/" className="hover:text-slate-600/[0.8] duration-200">
         <FontAwesomeIcon icon={faHouse} />
       </a>
       <span className="italic select-none">Yulgeon Menu Recommendation</span>
-      <a
-        href="/"
-        className="text-lg bg-white rounded-[16px] p-3 text-center hover:bg-[#e1e1e1] duration-150"
-      >
-        <FontAwesomeIcon icon={faUser} className="mr-2" />
-        <span>My Page</span>
-      </a>
+      {login ? (
+        <a
+          href="/"
+          className="text-lg bg-white rounded-[16px] p-3 text-center hover:bg-[#e1e1e1] duration-150"
+        >
+          <FontAwesomeIcon icon={faUser} className="mr-2" />
+          <span>My Page</span>
+        </a>
+      ) : (
+        <div className="flex">
+          <button className="text-xl mr-6 hover:text-[#111111]/[0.5] duration-100">
+            Login
+          </button>
+          <button className="text-xl hover:text-[#111111]/[0.5] duration-100">
+            Sign up
+          </button>
+        </div>
+      )}
     </div>
   );
 };
